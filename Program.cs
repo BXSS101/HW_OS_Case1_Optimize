613268b0983f2fb24c740c4cc7e81995bc406882
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2021-10-07 18:30:48 +0700

    Make it 4 Threads, but slower
    Just add more Thread, no other optimization

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ using System.Threading;
 namespace Problem01 {
     class Program {
         static byte[] Data_Global = new byte[1000000000];
-        static long[] Sum_Global = {0, 0};
-        static int G_index = 0;
+        static long[] Sum_Global = {0, 0, 0, 0};
 
         static int ReadData() {
             int returnData = 0;
@@ -43,15 +42,22 @@ namespace Problem01 {
                 Sum_Global[n] += (Data_Global[ind] / 3);
             }
             Data_Global[ind] = 0; 
-            
         }
         static void newThreadA() {
-            for (int i = 0; i < 500000000; i++) 
+            for (int i = 0; i < 250000000; i++) 
                 sum(i,0);
         }
         static void newThreadB() {
-            for (int j = 500000000; j < 1000000000; j++)
+            for (int j = 250000000; j < 500000000; j++)
                 sum(j,1);
+        }
+        static void newThreadC() {
+            for (int i = 500000000; i < 750000000; i++) 
+                sum(i,2);
+        }
+        static void newThreadD() {
+            for (int j = 750000000; j < 1000000000; j++)
+                sum(j,3);
         }
         static void Main(string[] args) {
             Stopwatch sw = new Stopwatch();
@@ -70,18 +76,24 @@ namespace Problem01 {
             Console.Write("\n\nWorking...");
             Thread th1 = new Thread(newThreadA);
             Thread th2 = new Thread(newThreadB);
+            Thread th3 = new Thread(newThreadC);
+            Thread th4 = new Thread(newThreadD);
             sw.Start();
             th1.Start();
             th2.Start();
+            th3.Start();
+            th4.Start();
             /*for (i = 0; i < 1000000000; i++)
                 sum();*/
             th1.Join();
             th2.Join();
+            th3.Join();
+            th4.Join();
             sw.Stop();
             Console.WriteLine("Done.");
 
             /* Result */
-            Console.WriteLine("Summation result: {0}", Sum_Global[0] + Sum_Global[1]);
+            Console.WriteLine("Summation result: {0}", Sum_Global[0] + Sum_Global[1] + Sum_Global[2] + Sum_Global[3]);
             Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
         }
     }
